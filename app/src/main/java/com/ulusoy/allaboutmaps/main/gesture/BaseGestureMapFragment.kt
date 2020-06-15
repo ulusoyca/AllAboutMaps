1ac0f4e1fa818ff4676494b902454649f62212cd
root <root@A191116465-2004.china.huawei.com>
2020-06-15 09:41:03 +0300

    add more logs

--- app/src/main/java/com/ulusoy/allaboutmaps/main/gesture/BaseGestureMapFragment.kt
+++ app/src/main/java/com/ulusoy/allaboutmaps/main/gesture/BaseGestureMapFragment.kt
@@ -24,24 +24,45 @@ import timber.log.Timber
 
 abstract class BaseGestureMapFragment : MapLifecycleHandlerFragment() {
 
+    private var numOfMoveStart = 0
+    private var numOfIdleCamera = 0
+    private var numOfCancel = 0
+
     protected fun onCameraIdle(mapProvider: MapProvider) {
         val providerName = requireContext().getString(mapProvider.providerNameResId)
         val event = requireContext().getString(R.string.camera_idle)
-        Timber.d("[MAP-${providerName}] onCameraIdle")
-        Toast.makeText(requireContext(), "$providerName - $event", Toast.LENGTH_SHORT).show()
+        numOfIdleCamera++
+        Timber.d("[MAP-${providerName}] ${getEventNumbers()}")
+        Toast.makeText(requireContext(), getToastNessage(providerName, event), Toast.LENGTH_SHORT).show()
     }
 
     protected fun onCameraMoveStared(mapProvider: MapProvider) {
         val providerName = requireContext().getString(mapProvider.providerNameResId)
         val event = requireContext().getString(R.string.camera_move_started)
-        Timber.d("[MAP-$providerName] onCameraMoveStared")
-        Toast.makeText(requireContext(), "$providerName - $event", Toast.LENGTH_SHORT).show()
+        numOfMoveStart++
+        Timber.d("[MAP-$providerName] ${getEventNumbers()}")
+        Toast.makeText(requireContext(), getToastNessage(providerName, event), Toast.LENGTH_SHORT).show()
     }
 
     protected fun onCameraMoveCanceled(mapProvider: MapProvider) {
         val providerName = requireContext().getString(mapProvider.providerNameResId)
         val event = requireContext().getString(R.string.camera_move_canceled)
-        Timber.d("[MAP-$providerName] onCameraMoveCanceled")
-        Toast.makeText(requireContext(), "$providerName - $event", Toast.LENGTH_SHORT).show()
+        numOfCancel++
+        Timber.d("[MAP-$providerName] ${getEventNumbers()}")
+        Toast.makeText(requireContext(), getToastNessage(providerName, event), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getToastNessage(providerName: String, event: String) =
+        "$providerName - $event\n${getEventNumbers()}"
+
+    override fun onResume() {
+        super.onResume()
+        numOfIdleCamera = 0
+        numOfMoveStart = 0
+        numOfCancel = 0
+    }
+
+    private fun getEventNumbers(): String {
+        return "Idle: $numOfIdleCamera\nMove Start: $numOfMoveStart\nCancel: $numOfCancel"
     }
 }
